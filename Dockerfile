d916db6ff7eda4dc8706332a518e3592be4a0d37
Svetoslav Batchovski <1735151+sveba@users.noreply.github.com>
2025-01-27 17:12:31 +0100

    move to async

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,8 @@ LABEL org.opencontainers.image.source = "https://github.com/sveba/easee2mqtt"
 WORKDIR /app
 
 COPY requirements.txt requirements.txt
-RUN pip3 install -r requirements.txt
+RUN pip3 install --upgrade pip && \
+    pip3 install -r requirements.txt
 
 COPY ./easee2mqtt.py /app/easee2mqtt.py
 
--- easee2mqtt.py
+++ easee2mqtt.py
@@ -1,228 +1,138 @@
-#!/usr/bin/python3
-import json
-import time
+import asyncio
+import aiomqtt
+import logging
 import os
 import sys
-import logging
-import requests
-import paho.mqtt.client as mqtt
 
+from pyeasee import Easee, STATUS
 
 LOGLEVEL = os.environ.get('LOGLEVEL', 'INFO').upper()
-easee_username = os.environ.get('EASEE_USERNAME', None)
-easee_password = os.environ.get('EASEE_PASSWORD', None)
-easee_chargers = os.environ.get("EASEE_CHARGERS").split(",")
 polling_interval = int(os.environ.get('POLLING_INTERVAL', 300))
-mqtt_host = os.environ.get('MQTT_HOST', None)
-mqtt_port = int(os.environ.get('MQTT_PORT', 1833))
-mqtt_password = os.environ.get('MQTT_PASSWORD', None)
-mqtt_username = os.environ.get('MQTT_USERNAME', None)
-ACCESS_TOKEN = None
-token_expiration = time.time()
-EASEE_API_BASE = "https://api.easee.cloud/api"
 
 logging.basicConfig(handlers=[logging.StreamHandler(sys.stdout)],
                     level=LOGLEVEL,
                     format="[%(asctime)s] %(levelname)s %(message)s",
                     datefmt='%Y-%m-%d %H:%M:%S')
-
-
-def check_access_token():
-    global ACCESS_TOKEN, token_expiration
-    if token_expiration - time.time() < 350:
-        logging.info(
-            "Token expires in less than 350 seconds. Fetching a new token.")
-        url = f"{EASEE_API_BASE}/accounts/login"
-
-        headers = {
-            "Accept": "application/json",
-            "Content-Type": "application/json-patch+json"
-        }
-
-        body = {
-            "userName": easee_username,
-            "password": easee_password
-        }
-
-        response = requests.request("POST", url, headers=headers, json=body)
-        logging.debug("Response from get_access_log: %s", response)
-        if response.status_code == 200:
-            logging.info("Successfully connected to Easee")
-        else:
-            logging.warning(
-                "Failed to connect to Easee. Response code: %s", response.status_code)
-            sys.exit(1)
-        json_obj = json.loads(response.text)
-        token_expiration = time.time() + json_obj['expiresIn']
-        ACCESS_TOKEN = json_obj['accessToken']
-        logging.info("Successfully retrieved and stored a new token.")
-    else:
-        logging.debug("Token is not up for refresh.")
-
-
-def response_codes(code):
-    msg = f"Unknown response code: {code}"
-    if code in [200, 202]:
-        msg = "Command successfully sent to charger"
-    elif code == 400:
-        msg = "Command has missing/invalid values"
-    elif code == 401:
-        msg = "Missing authorization data. Check 'Authorization' header"
-    elif code == 403:
-        msg = "Forbidden. Authorization set, but access to resource is denied"
-    elif code == 415:
-        msg = "Payload format is in an unsupported format"
-    elif code == 500:
-        msg = "Oops! Unexpected internal error. Request has been logged and code monkeys warned"
-    elif code == 503:
-        msg = "Server gateway cannot reach API. Try again in about a minute..."
-    elif code == 504:
-        msg = "Unable to deliver commands upstream. End device is not reachable, or a problem with queueing the device command"
-    return msg
+cur_charger = None
 
 
 def convert_to_af(code):
-    af = f"Unknown state code: {code}"
-    if code in [0, 1]:
+    af = "B"
+    if code in [STATUS[1], STATUS[0]]:
         af = "A"
-    elif code == 3:
+    elif code == STATUS[3]:
         af = "C"
-    else:
-        af = "B"
     return af
 
 
-def get_state(charger_id):
-    check_access_token()
-    url = f"{EASEE_API_BASE}/chargers/{charger_id}/state"
-    headers = {
-        "Accept": "application/json",
-        "Authorization": "Bearer " + ACCESS_TOKEN}
-    resp = requests.request("GET", url=url, headers=headers)
-    parsed = resp.json()
-    logging.debug("State")
-    logging.debug(parsed)
-    if resp.status_code != 200:
-        logging.warning(
-            "Response code %s when trying to get_state", resp.status_code)
-    return parsed
+async def setupCharger():
+    global cur_charger
+    easee_username = os.environ.get('EASEE_USERNAME', None)
+    easee_password = os.environ.get('EASEE_PASSWORD', None)
+    easee_charger = os.environ.get("EASEE_CHARGER")
+    easee = Easee(easee_username, easee_password)
+
+    sites = await easee.get_sites()
+    for site in sites:
+        logging.debug("Site %s (%s)", site.name, site.id)
+        circuits = site.get_circuits()
+        for circuit in circuits:
+            logging.debug("Circuit %s ", circuit.id)
+            chargers = circuit.get_chargers()
+            for charger in chargers:
+                logging.debug("Charger Config is enabled: %s",
+                              charger.__dict__)
+                logging.debug("Charger %s (%s).", charger.name, charger.id)
+                if charger.id in easee_charger:
+                    logging.info("Charger found")
+                    cur_charger = charger
+                    break
 
 
-def publish_state(client, charger):
-    state = get_state(charger)
-    config = get_config(charger)
-    client.publish(
-        f"easee2MQTT/{charger}/charging_enabled", config['isEnabled'])
-    client.publish(
-        f"easee2MQTT/{charger}/charging_current", state['dynamicChargerCurrent'])
-    client.publish(f"easee2MQTT/{charger}/chargerOpMode",
-                   convert_to_af(state['chargerOpMode']))
+async def refreshCharger(client, mqtt_root_topic):
+    while True:
+        await publish_state(client, mqtt_root_topic)
+        await asyncio.sleep(polling_interval)
 
 
-def on_message(client, userdata, message):
-    logging.info("Message received on topic: %s, payload: %s",
-                 message.topic, str(message.payload.decode('utf-8')))
-    callback_topic, resp = None, None
-    charger = message.topic.split("/")[1]
-    headers = {
-        "Accept": "application/json",
-        "Authorization": "Bearer " + ACCESS_TOKEN}
-
-    if message.topic.split("/")[2] == "charging_enabled":
-        url = f"{EASEE_API_BASE}/chargers/{charger}/settings"
-        if (str(message.payload.decode("utf-8")).casefold() == "true" or
-                str(message.payload.decode("utf-8")).casefold() == "false"):
-            data = {
-                'enabled': str(message.payload.decode("utf-8")).title()
-            }
-            resp = requests.post(url, headers=headers, json=data)
-            callback_topic = f"easee2MQTT/{charger}/charging_enabled"
-
-        else:
-            logging.warning(
-                "Couldn't identify payload. 'true' or 'false' is only supported values.")
-
-    elif message.topic.split("/")[2] == "charging_current":
-        if float(message.payload.decode('utf-8')) < 33.0:
-            url = f"{EASEE_API_BASE}/chargers/{charger}/settings"
-            data = {
-                "dynamicChargerCurrent": message.payload.decode('utf-8')
-            }
-            resp = requests.post(url, headers=headers, json=data)
-            callback_topic = f"easee2MQTT/{charger}/charging_current"
-        else:
-            logging.warning("Couldn't publish new charging_current")
-    if callback_topic is not None:
-        try:
-            if message.topic.split("/")[2] != "ping":
-                logging.info("Manually publishing setting %s for %s",
-                             message.topic.split('/')[2], charger)
-                client.publish(callback_topic, message.payload.decode('utf-8'))
-        except:
-            logging.warning(
-                "Couldn't publish manually for message: %s", message)
-
-    if resp is not None:
-        try:
-            # Log a warning if we still have a status_code
-            if resp.status_code in [200, 202]:
-                logging.info("Response %s - Payload: %s",
-                             response_codes(resp.status_code), message.payload.decode('utf-8'))
-            else:
-                logging.warning("Failed to send command to charger. Response code %s - %s",
-                                resp.status_code, response_codes(resp.status_code))
-        except:
-            logging.warning(
-                "No status_code from received message: %s", message)
+async def listen(tg):
+    mqtt_host = os.environ.get('MQTT_HOST', None)
+    mqtt_port = int(os.environ.get('MQTT_PORT', 1883))
+    mqtt_password = os.environ.get('MQTT_PASSWORD', None)
+    mqtt_username = os.environ.get('MQTT_USERNAME', None)
+    mqtt_root_topic = os.environ.get('MQTT_ROOT_TOPIC', "easee2MQTT")
+    logging.info("Connecting to MQTT broker %s:%s", mqtt_host, mqtt_port)
+    async with aiomqtt.Client(hostname=mqtt_host, port=mqtt_port, username=mqtt_username, password=mqtt_password) as client:
+        tg.create_task(refreshCharger(client, mqtt_root_topic))
+        await client.subscribe(f"{mqtt_root_topic}/cmnd/#")
+        async for message in client.messages:
+            await on_message(message)
+            await asyncio.sleep(5)
+            await publish_state(client, mqtt_root_topic)
 
 
-def get_config(charger):
-    url = f"{EASEE_API_BASE}/chargers/{charger}/config"
-    headers = {
-        "Accept": "application/json",
-        "Authorization": "Bearer " + ACCESS_TOKEN}
-    resp = requests.request("GET", url=url, headers=headers)
-    parsed = resp.json()
-    if resp.status_code != 200:
-        logging.warning(
-            "Response code %s when trying to get_config", resp.status_code)
-    return parsed
+async def on_message(message):
+    logging.debug("Message received on topic: %s, payload: %s",
+                  message.topic, str(message.payload.decode('utf-8')))
+    payload = message.payload.decode('utf-8')
+
+    if message.topic.matches("easee2mqtt/+/cmnd/charging"):
+        logging.info("Received command to enable/disable charging")
+        await enable(str(payload).casefold() == "true")
+
+    if message.topic.matches("easee2mqtt/+/cmnd/current"):
+        logging.info("Received command to set current")
+        if int(payload) < 33:
+            await set_current(int(payload))
 
 
-def main():
-    logging.info("Script is starting. Looking for settings")
-    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, "Easee2MQTT")
-    logging.debug("MQTT: Connect to %s:%s", mqtt_host, mqtt_port)
-    if mqtt_password:
-        client.username_pw_set(username=mqtt_username, password=mqtt_password)
-    client.connect(mqtt_host, mqtt_port)
-    client.loop_start()
+async def enable(enable: bool):
+    charger_config = await cur_charger.get_config()
+    logging.debug("Charger Config: %s", charger_config.__dict__)
+    enabling_required = enable and not charger_config["isEnabled"]
 
-    for charger in easee_chargers:
-        logging.info("Subscribing to topics for charger %s.", charger)
-        client.subscribe(f"easee2MQTT/{charger}/charging_enabled/set")
-        client.subscribe(f"easee2MQTT/{charger}/charging_current/set")
-    client.on_message = on_message
+    if enabling_required:
+        logging.debug("Enabling charger")
+        await cur_charger.enable_charger(enable)
 
-    try:
-        while True:
-            for charger in easee_chargers:
-                try:
-                    logging.debug(
-                        "Fetching and publishing latest stats of %s", charger)
-                    publish_state(client, charger)
-                except Exception as err:
-                    logging.error(err)
-                    logging.error(
-                        "Failed to fetch and publish new stats of %s. Will retry in %s seconds", charger, polling_interval)
-            time.sleep(polling_interval)
-    except Exception as err:
-        logging.error(err)
-    client.loop_stop()
+    charger_state = await cur_charger.get_state()
+    logging.debug("Charger State: %s", charger_state.__dict__)
+    if charger_state["chargerOpMode"] in [STATUS[1], STATUS[7]]:
+        logging.warning("Charger can not be paused/resumed in this state")
+        return
+
+    if enable:
+        logging.info("Resume charger")
+        await cur_charger.smart_charging(True)
+        await cur_charger.resume()
+    else:
+        logging.info("Pause charger")
+        await cur_charger.smart_charging(False)
+        await cur_charger.pause()
 
 
-if __name__ == "__main__":
-    try:
-        main()
-    except KeyboardInterrupt:
-        print("Exiting program")
+async def publish_state(client, mqtt_root_topic):
+    logging.info("Publishing charger status")
+
+    state = await cur_charger.get_state()
+    config = await cur_charger.get_config()
+    logging.debug("Charger state: %s", state.__dict__)
+    logging.debug("Charger config: %s", config.__dict__)
+
+    await client.publish(f"{mqtt_root_topic}/charging_enabled", state["smartCharging"])
+    await client.publish(f"{mqtt_root_topic}/charging_current", state["dynamicChargerCurrent"])
+    await client.publish(f"{mqtt_root_topic}/chargerOpMode", convert_to_af(state["chargerOpMode"]))
+
+
+async def set_current(current: int):
+    logging.info("Set dynamic current: %s", current)
+    await cur_charger.set_dynamic_charger_current(current)
+
+
+async def main():
+    await setupCharger()
+    # Use a task group to manage and await all tasks
+    async with asyncio.TaskGroup() as tg:
+        tg.create_task(listen(tg))  # Start the listener task
+
+asyncio.run(main())
